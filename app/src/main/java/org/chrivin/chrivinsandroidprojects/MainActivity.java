74fe0ff61ca7d7d5b7a465146a5cff6ef35ee4eb
Christofel Kevin <116001020+christofelkev@users.noreply.github.com>
2023-01-02 14:53:09 +0700

    add my new android project 'Bucket List App'

--- app/src/main/java/org/chrivin/chrivinsandroidprojects/MainActivity.java
+++ app/src/main/java/org/chrivin/chrivinsandroidprojects/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
                 new Project("Motivational Quote", "Making a simple change to the layout, with my own favourite quote!",R.drawable.quote),
                 new Project("BMI Calculator", "A real life working BMI calculator teaching me Variables, Methods, and Conditional Logic",R.drawable.tape),
                 new Project("Inches Converter", "A basic converter to convert Inches to meters (My personal calculator app made on my own)",R.drawable.calculator),
-                new Project("Ichiraku Ramen", "A menu app for a fictional restaurant in Naruto's anime, learning about Activities, Classes & Objects, Arrays, Intents, and Listview",R.drawable.ichiraku)
+                new Project("Ichiraku Ramen", "A menu app for a fictional restaurant in Naruto's anime, learning about Activities, Classes & Objects, Arrays, Intents, and Listview",R.drawable.ichiraku),
+                new Project("Bucket List App", "A bucket list app , teaches me about new layout style like 'gradient','weight','@drawableRes' Annotation  ",R.drawable.androiddev)
         };
 
         ProjectsAdapter adapter = new ProjectsAdapter(projects);
